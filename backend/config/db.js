3640b29dba6534abe92fd1127a124d5b20ac65ae
jane200680 <jamedinaca@uide.edu.ec>
2026-07-23 07:49:57 -0500

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2/promise";
-
-export const db = await mysql.createConnection({
-  host: process.env.DB_HOST || "mysql",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "2026",
-  database: process.env.DB_NAME || "memora",
-});
